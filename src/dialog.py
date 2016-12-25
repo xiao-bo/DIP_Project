cdd986bc597f737b7fcc72e6a0263529fb2eee73
xiao-bo <ay852852@gmail.com>
2016-12-25 11:16:21 +0800

    split dialog class from main

--- src/dialog.py
+++ src/dialog.py
@@ -1,32 +1,19 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import pygame
-from pygame.locals import *
-import sys
 
-SCREEN_SIZE = (640, 480)
 
-pygame.init()
-screen = pygame.display.set_mode(SCREEN_SIZE)
-pygame.display.set_caption("Hello, world!")
+dialogContent=[
+"hello welcome to XX, please smile",\
+"your smile is good ",\
+"explaing smile",\
+"Do you want experience school? ",\
+"school....",\
+"bug hole will open",\
+"Goodbye"
+]##index from 0 to 6
 
-# 文字格式
-font = pygame.font.SysFont(None, 80)
-# 列出文字（第1引数列出文字、第2引数寫True文字看的比較順、第3引数彩色、第4引数背景顔色） 
-hello1 = font.render("Hello,    world!", False, (0,0,0))
-hello2 = font.render("Hello, world!", True, (0,0,0))
-hello3 = font.render("Hello, world!", True, (255,0,0), (255,255,0))
+class Dialog:
+	def __init__(self):
+		pass
 
-while True:
-  screen.fill((0,0,255))
-
-  # 顕示在印幕上
-  screen.blit(hello1, (20,50))
-  screen.blit(hello2, (20,150))
-  screen.blit(hello3, (20,250))
-
-  pygame.display.update()
-
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      sys.exit()
+	def load(self,index):
+		text = dialogContent[index]
+		return text
--- src/main.py
+++ src/main.py
@@ -5,8 +5,9 @@ import pygame
 from pygame.locals import *
 from pgu import gui
 
-### other class in src
+### import other class in src
 from npc import NPC
+from dialog import Dialog
 
 
 
@@ -15,12 +16,16 @@ class EnterPoint:
         ### initial game's property value
         self.running = True
         self.screen = None
-        self.size = self.width, self.height = 840, 600 ## window size
-        self.background=255,255,255 ##background color 
 
+        ## window size
+        self.size = self.width, self.height = 840, 600 
 
-        
-       
+        ##background color 
+        self.background = 255,255,255 
+
+        ## dialog index
+    	self.index = 0
+    	
     def on_init(self):
         ## game initail process
         pygame.init()  
@@ -30,22 +35,54 @@ class EnterPoint:
         pygame.display.set_caption("WindowTitle")
 
         ## load minon NPC
-        npcMinion=NPC("../pic/npc_minion.jpeg")
+        npcMinion = NPC("../pic/npc_minion.jpeg")
         self.npcMinion,self.npcMinion_rect=npcMinion.load()
 
-        
+        ## init dialog font 
+        self.font = pygame.font.SysFont(None, 80)
+       
+
     def on_event(self, event):
+        dialog = Dialog()
+        self.text = dialog.load(self.index)
 
         if event.type == pygame.QUIT:
             self.running = False
-        
+        ## detect keydown
+        elif event.type == pygame.KEYDOWN:
+        	## if key is right
+            if event.key == pygame.K_RIGHT: 
+            	## change dialog
+                self.index = (self.index+1)%5 
+            elif event.key == pygame.K_LEFT:
+                self.index = (self.index-1)%5
+
+            ## simulate person is leaving state.    
+            elif event.key == pygame.K_BACKSPACE: 
+            	## text is "goodbye"
+            	self.index = 6  
+            ## simulate person appears in another side	
+            elif event.key == pygame.K_SPACE: 
+            	## text is "bug hole will opening"
+            	self.index = 5
+            ## restart dialog  
+            elif event.key == pygame.K_ESCAPE:
+            	##
+            	self.index = 0
+
+            self.text = dialog.load(self.index)
+            self.hello = self.font.render(self.text, False, (0,0,0))
+
     def on_loop(self):
-       
+    	## FaceDetect put it.
+        
+
         pass
 
     def on_render(self):
         self.screen.fill(self.background)
         self.screen.blit(self.npcMinion, self.npcMinion_rect)
+        self.screen.blit(self.hello, (20,300))
         pygame.display.flip()
         
 
@@ -57,8 +94,8 @@ if __name__ == "__main__" :
 
     Enter = EnterPoint()
     
-    if Enter.on_init()==False:
-    	Enter.running=False
+    if Enter.on_init() == False:
+    	Enter.running = False
 
     while( Enter.running ):
         for event in pygame.event.get():
