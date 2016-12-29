ec751441886d2b558475d26d70d1c6a86e1601a0
yuasuka <yuasuka@gmail.com>
2016-12-30 07:13:38 +0800

    date 12.30 morning , newest

--- src/skinPimpleDetect.py
+++ src/skinPimpleDetect.py
@@ -24,7 +24,11 @@ while True:
     skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
     #skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
     skin = cv2.bitwise_and(frame, frame, mask = skinMask)
-
+    mask2 = np.zeros((skinMask.shape[0],skinMask.shape[1]))
+    #print(mask2.shape)
+    #print(skinMask.shape)
+    skin2 = cv2.blur(skin, (5, 5))
+    frame2 = np.maximum(frame, skin2)
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.3,
@@ -34,20 +38,6 @@ while True:
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        skin2=cv2.GaussianBlur(skin, (5,5),0)
-        frame2 = frame-skin+skin2
-    #print(converted.shape)
-        '''for b in range(x, x+w):
-            #print("b", b)
-            for a in range(y, y+h):
-                #print("a",a)
-                roi = converted[y:(y+10), x:(x+10)]
-                color0 = np.mean(roi[0])
-                color1 = np.mean(roi[1])
-                color2 = np.mean(roi[2])
-                #print("color shape", color0)
-                if color0<10 and color1>70 and color2>50: #converted[a,b,0]< 10 and converted[a,b,1] > 70 and converted[a,b,2] > 50:
-                    cv2.rectangle(frame, (b, a), (b+10, a+10), (0, 255, 255), 2)'''
     # Display the resulting frame
     
     cv2.imshow('Video', np.hstack([frame, frame2, skin]))
